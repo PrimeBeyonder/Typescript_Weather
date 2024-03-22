c61649c9c9c1f77e5f8e4b9823cd5ef01df456ab
Andrew <hugomyat37@gmail.com>
2024-03-22 16:23:24 +0630

    fetch function added

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { MainWrapper } from "./weather.module";
 import { AiOutlineSearch } from "react-icons/ai";
 import { WiHumidity } from "react-icons/wi";
@@ -31,6 +31,32 @@ interface WeatherDataProps {
 
 }
 const DisplayWeather = () => {
+    const api_key = "07c569eed39ebaf09b115ee1dda0a89f";
+    const api_Endpoint = `https://api.openweathermap.org/data/3.0/`;
+    const [weatherData, setWeatherData] = useState<WeatherDataProps | null>(null);
+    const [isLoading, setLoading] = useState(false);
+    const [searchCity, setSearchCity] = useState("");
+
+
+    const fetchCurrentWeather = useCallback
+        (async (lat: number, lon: number) => {
+            const url = `${api_Endpoint}weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
+            const res = await axios.get(url);
+            return res.data;
+        }, [api_Endpoint, api_key]);
+
+    const fetchWeatherData = async (city: string) => {
+        try {
+            const url = `${api_Endpoint}weather?q=${city}&appid=${api_key}&units=metric`;
+            const searchResponse = await axios.get(url);
+            const currentWeatherData: WeatherDataProps = searchResponse.data;
+            return { currentWeatherData };
+        } catch (error) {
+            throw error;
+        };
+    };
+    
+
     return (
         <div>DisplayWeather</div>
     )
