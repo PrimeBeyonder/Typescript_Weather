8ac222931b8d724d2542f9fe98aedc3d9803d64f
Andrew <hugomyat37@gmail.com>
2024-03-22 15:38:25 +0630

    icon and interface added

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -1,5 +1,35 @@
-import React from 'react'
+import React from "react";
+import { MainWrapper } from "./weather.module";
+import { AiOutlineSearch } from "react-icons/ai";
+import { WiHumidity } from "react-icons/wi";
+import { SiWindicss } from "react-icons/si";
+import {
+    BsFillSunFill,
+    BsCloudyFill,
+    BsFillCloudRainFill,
+    BsCloudFog2Fill,
+} from "react-icons/bs";
+import { RiLoaderFill } from "react-icons/ri";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import axios from "axios";
 
+interface WeatherDataProps {
+    name: string;
+    main: {
+        temp: number;
+        humidity: number;
+    };
+    sys: {
+        country: string;
+    };
+    weather: {
+        main: string;
+    }[];
+    wind: {
+        speed: number;
+    };
+
+}
 const DisplayWeather = () => {
     return (
         <div>DisplayWeather</div>
